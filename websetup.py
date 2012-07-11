4bb5e1c18b13409457e9fdd2f4098a780974dbb4
Fabio Erculiani <lxnay@sabayon.org>
2012-07-11 10:36:04 +0200

    Revert "[www.sabayon.org] add SIGUSR2 signal handler to www"
    This reverts commit 6a0dc6d8c246d2f790b742b9283bef4209e32828.

--- websetup.py
+++ websetup.py
@@ -8,27 +8,6 @@ from www.config.environment import load_environment
 
 log = logging.getLogger(__name__)
 
-import gc
-import sys
-import signal
-
-gc.set_debug(gc.DEBUG_STATS)
-# Run gc.collect() with stats to stderr on SIGUSR2
-def _gc_collect(signum, frame):
-    # shows all the memleak info and stores
-    # uncollectable objects to gc.garbage
-    gc.set_debug(gc.DEBUG_LEAK)
-    gc.collect()
-    sys.stderr.write("Uncollectable objects:\n")
-    sys.stderr.write("%s\n" % (gc.garbage,))
-    sys.stderr.write("\n---\n")
-    gc.set_debug(gc.DEBUG_STATS)
-    # now clear for real
-    del gc.garbage[:]
-    gc.collect()
-
-signal.signal(signal.SIGUSR2, _gc_collect)
-
 def setup_config(command, filename, section, vars):
     """Place any commands to setup www here"""
     conf = appconfig('config:' + filename)
