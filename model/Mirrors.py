99b5b343e7520dfb091381b3899a562158b534cb
Fabio Erculiani <lxnay@sabayon.org>
2012-09-16 08:46:04 +0200

    [model/Mirrors] continent_mirrors: return both http and ftp URLs

--- model/Mirrors.py
+++ model/Mirrors.py
@@ -22,7 +22,7 @@ class Mirrors(Database):
         """
         self.execute_query("""
         SELECT
-            http_t.field_mirror_http_uri_url as http_url
+            http_t.field_mirror_http_uri_url as url
         FROM
             field_data_field_mirror_http_uri as http_t,
             field_data_field_use_in_router as router_t,
@@ -33,5 +33,22 @@ class Mirrors(Database):
             http_t.entity_id = continent_t.entity_id AND
             router_t.field_use_in_router_value = "1" AND
             router_t.deleted = "0"
-        """, (continent,))
-        return [x['http_url'] for x in self.fetchall()]
+
+        UNION ALL
+
+        SELECT
+            ftp_t.field_mirror_ftp_uri_url as url
+        FROM
+            field_data_field_mirror_ftp_uri as ftp_t,
+            field_data_field_use_in_router as router_t,
+            field_data_field_continent as continent_t
+        WHERE
+            continent_t.field_continent_value = %s AND
+            ftp_t.entity_id = router_t.entity_id AND
+            ftp_t.entity_id = continent_t.entity_id AND
+            router_t.field_use_in_router_value = "1" AND
+            router_t.deleted = "0"
+        """, (continent, continent,))
+        data = self.fetchall()
+        mirrors = [x['url'] for x in data]
+        return mirrors
